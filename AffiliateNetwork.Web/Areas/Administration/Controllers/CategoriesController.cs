1f07ae34ee36405e2a675278a0b9e04a275645e7
Dimitar Kostov <d.kostov.dev@gmail.com>
2014-11-05 17:11:33 +0200

    Paging and sorting

--- AffiliateNetwork.Web/Areas/Administration/Controllers/CategoriesController.cs
+++ AffiliateNetwork.Web/Areas/Administration/Controllers/CategoriesController.cs
@@ -3,20 +3,75 @@
     using System.Linq;
     using System.Net;
     using System.Web.Mvc;
+    using System;
+    using System.Linq.Dynamic;
 
     using AffiliateNetwork.Contracts;
     using AffiliateNetwork.Models;
+    using AffiliateNetwork.Web.Areas.Administration.Models;
+    using AffiliateNetwork.Web.Infrastructure.Filters;
+    
     
     public class CategoriesController : BaseController
     {
+        private const int defaultPageSize =1;
+
         public CategoriesController(IDataProvider provider)
             : base(provider)
         {
         }
 
-        public ActionResult Index()
+        public ActionResult IndexNew(string sortDirection = "ASC", int? page = 1, string sort = "Id")
         {
-            return View(this.Data.Categories.All().ToList());
+            // string sort = "Id", string sortDirection, int? page
+            //var currentPage = page ?? 1;
+            //ViewData["SortItem"] = sort;
+            //sort = sort ?? "Id";
+            //ViewData["CurrentPage"] = currentPage;
+            //ViewData["TotalPages"] = (int)Math.Ceiling((float)this.Data.Categories.All().Count() / _pageSize);
+
+            //var products = this.Data.Categories.All().Select(ListCategoryViewModel.ViewModel);
+
+            //var sortedProducts = products
+            //    .OrderBy(sort)
+            //    .Skip((currentPage - 1) * _pageSize)
+            //    .Take(_pageSize);
+
+            var products = this.Data.Categories.All().Select(ListCategoryViewModel.ViewModel);
+            return View(products);
+        }
+
+        public ActionResult Index(string sortBy, string sortDirection, string searchString, int? page)
+        {
+            sortBy = string.IsNullOrEmpty(sortBy) ? "Id" : sortBy;
+            ViewBag.SortDirection = sortDirection == "ASC" ? "DESC" : "ASC";
+            var currentPage = page ?? 1;
+            ViewBag.CurrentPage = currentPage;
+            ViewBag.TotalPages= (int)Math.Ceiling((float)this.Data.Categories.All().Count() / defaultPageSize);
+            ViewBag.CurrentSort = sortBy;
+
+            var selectedItems = this.Data.Categories.All();
+
+            if (!String.IsNullOrEmpty(ViewBag.SearchString))
+            {
+                string searchVar = ViewBag.SearchString;
+                selectedItems = selectedItems.Where(x => x.Name.ToLower().Contains(searchVar.ToLower()));
+            }
+
+            if (ViewBag.SortDirection == "DESC")
+            {
+                selectedItems = selectedItems.OrderBy(sortBy + " " + "descending");
+            }
+            else
+            {
+                selectedItems = selectedItems.OrderBy(sortBy);
+            }
+
+            selectedItems = selectedItems
+                .Skip((currentPage - 1) * defaultPageSize)
+                .Take(defaultPageSize);
+
+            return View(selectedItems.ToList());
         }
 
         public ActionResult Create()
---
+++ AffiliateNetwork.Web/Infrastructure/Helpers/PaginationHelper.cs
@@ -0,0 +1,75 @@
+﻿namespace System.Web.Mvc.Html
+{
+    using System.Text;
+
+    public static class PageLinkHelper
+    {
+        public static IHtmlString PageLink(this HtmlHelper html, int currentPage, int totalPages, Func<int, string> pageUrl)
+        {
+            var diff = 1;
+            StringBuilder result = new StringBuilder();
+            var TotalPages = totalPages;
+
+            if (currentPage < 1)
+            {
+                currentPage = 1;
+            }
+
+            if ((currentPage + diff) < totalPages)
+            {
+                totalPages = currentPage + diff;
+            }
+                
+            var startPage = 1;
+
+            if ((currentPage - diff) > startPage)
+            {
+                startPage = currentPage - diff;
+            }
+
+            if ((currentPage - diff) > 1)
+            {
+                TagBuilder tag3 = new TagBuilder("a");
+                tag3.Attributes.Add("href", pageUrl(1));
+                tag3.InnerHtml = "1";
+                result.AppendLine(tag3.ToString());
+            }
+
+            if ((currentPage - (diff + 1)) > 1)
+            {
+                TagBuilder tag2 = new TagBuilder("a");
+                tag2.Attributes.Add("href", pageUrl(currentPage - (diff + 1)));
+                tag2.InnerHtml = "...";
+                result.AppendLine(tag2.ToString());
+            }
+
+            for (int i = startPage; i <= totalPages; i++)
+            {
+                TagBuilder tag = new TagBuilder("a");
+                tag.Attributes.Add("href", pageUrl(i));
+                tag.InnerHtml = i.ToString();
+                if (i == currentPage)
+                    tag.AddCssClass("pageSelected");
+                result.AppendLine(tag.ToString());
+            }
+
+            if ((currentPage + (diff + 1)) < TotalPages)
+            {
+                TagBuilder tag2 = new TagBuilder("a");
+                tag2.Attributes.Add("href", pageUrl(currentPage + (diff + 1)));
+                tag2.InnerHtml = "...";
+                result.AppendLine(tag2.ToString());
+            }
+
+            if ((currentPage + diff) < TotalPages)
+            {
+                TagBuilder tag3 = new TagBuilder("a");
+                tag3.Attributes.Add("href", pageUrl(TotalPages));
+                tag3.InnerHtml = TotalPages.ToString();
+                result.AppendLine(tag3.ToString());
+            }
+            
+            return new HtmlString(result.ToString());
+        }
+    }
+}
