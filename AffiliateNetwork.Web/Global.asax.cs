3ca43ac619bfd943dde797692a0fae22c97866e7
d-kostov-dev <d.kostov.dev@gmail.com>
2014-11-08 02:51:35 +0200

    Implemented Automapper

--- AffiliateNetwork.Web/Global.asax.cs
+++ AffiliateNetwork.Web/Global.asax.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using AffiliateNetwork.Infrastructure.Mapping;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -18,6 +20,9 @@ namespace AffiliateNetwork.Web
             BundleConfig.RegisterBundles(BundleTable.Bundles);
 
             ViewEnginesConfig.RegisterViewEngines(ViewEngines.Engines);
+
+            var autoMapperConfig = new AutoMapperConfig(Assembly.GetExecutingAssembly());
+            autoMapperConfig.Execute();
         }
     }
 }
