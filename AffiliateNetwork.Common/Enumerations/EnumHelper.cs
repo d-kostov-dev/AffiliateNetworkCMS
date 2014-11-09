6cb3cdc035481080b6a9d7f43473086f0ade2626
d-kostov-dev <d.kostov.dev@gmail.com>
2014-11-10 01:56:39 +0200

    Deleted unneeded class

--- AffiliateNetwork.Common/Enumerations/EnumHelper.cs
+++ AffiliateNetwork.Common/Enumerations/EnumHelper.cs
@@ -6,15 +6,6 @@
     using System.Linq;
     using System.Reflection;
 
-    public static class EnumExtensions
-    {
-        public static string ToDescription(this Enum e)
-        {
-            var attributes = (DisplayAttribute[])e.GetType().GetField(e.ToString()).GetCustomAttributes(typeof(DisplayAttribute), false);
-            return attributes.Length > 0 ? attributes[0].Description : string.Empty;
-        }
-    }
-
     public static class EnumHelper<T>
     {
         public static IList<T> GetValues(Enum value)
